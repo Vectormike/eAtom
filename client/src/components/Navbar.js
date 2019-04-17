f8557870351a08abb344f329b1868b2d221dbd4d
Vectormike40 <victor.junior40@yahoo.com>
2019-04-17 14:57:50 +0100

    first commit here

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
+
+class Navabr extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            open: false
+        }
+    }
+
+    toggle() {
+        this.setState({open: !this.state.open})
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>    
+        )
+    }
+}
+
+export default Navbar;
