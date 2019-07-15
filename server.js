9205429b340929c78a9fa19e15ae8f6ad289b9f4
vectormike40 <victorjonah199@gmail.com>
2019-07-15 16:40:39 +0100

    ...

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
 const app = express();
 
 app.use(bodyParser.json());
 
-//Test
+// Test
 app.get('/api/details', (req, res) => {
-    const details = [
-        {id: 1, name: "Victor"}    
-    ];
-res.json(details)
-})
+  const details = [{ id: 1, name: 'Victor' }];
+  res.json(details);
+});
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
-app.listen(PORT, () => (console.log(`Server running on port ${PORT}`)));
+app.listen(PORT, 
+  () => 
+  console.log(`Server running on port ${PORT}`));
