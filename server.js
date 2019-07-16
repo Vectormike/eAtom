d9a6c591b5f8ca7930810594d2f06334f86d4025
vectormike40 <victorjonah199@gmail.com>
2019-07-16 11:34:17 +0100

    Cleanup

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
+/* eslint-disable no-console */
 import express from 'express';
 
 import bodyParser from 'body-parser';
 
 import db from './server/database/dbConfig';
-import route from './server/routes/index';
+
+// Routes
+import router from './server/routes/index';
+
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Enforce Body parser
 app.use(bodyParser.json());
 
-// Test connection
+// Test database connection
 db.authenticate()
   .then(() => {
     console.log('Connection is OK');
   })
   .catch(err => {
-    console.log('Unable to connect');
+    console.log(`Unable to connect ${err}`);
   });
 
 // Test
-app.get('/api/details', (req, res) => {
+app.get('/test', (req, res) => {
   const details = [{ id: 1, name: 'Victor' }];
   res.json(details);
 });
 
 // Home router
-app.use('api', route)
+app.use('/', router)
+
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
