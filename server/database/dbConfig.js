73357ed536a5452d175ea85e5d4d697482d101a1
vectormike40 <victorjonah199@gmail.com>
2019-07-17 16:40:56 +0100

    Cleanup

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
 
-module.exports = new Sequelize('eAtom', 'postgres', '0000', {
-  host: 'localhost',
-  dialect: 'postgres',
-  operatorsAliases: false,
+module.exports = new Sequelize(
+  'eAtom',
+  'postgres',
+  '0000',
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
-});
+);
