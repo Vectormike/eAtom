e58bbf987cff165c0c53844dbfdda8fa5e6c58be
vectormike40 <victorjonah199@gmail.com>
2019-07-19 16:34:03 +0100

    Updated model

--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,27 @@
-import { DataTypes } from 'sequelize';
-import db from '../database/dbConfig';
 
-const User = db.define('users', {
-  lastName: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-  firstName: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    required: true,
-  },
-});
 
-export default User;
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    lastName: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  User.associate = function(models) {};
+  return User;
+};
