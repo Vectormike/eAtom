168a7912f9c08bf45a1f0d03b9a42fea6a7156ba
vectormike40 <victorjonah199@gmail.com>
2019-07-23 16:19:16 +0100

    Configured

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -31,20 +31,21 @@ const res = dotenv.config();
 if (res.error) {
   throw res.error;
 }
-console.log(res.parsed);
 
 
 let connectionString;
 if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.DATABASE_URL;
+  connectionString = process.env.DB_URL;
 } else if (process.env.NODE_ENV === 'development') {
-  connectionString = process.env.DATABASE_URL;
+  connectionString = process.env.DB_URL;
 } else {
-  connectionString = process.env.DATABASE_URL;
+  connectionString = process.env.DB_URL;
 }
 
 // Create new instance
 const pool = new Pool({ connectionString });
+// console.log(pool)
+
 
 // Query function
 export default {
@@ -52,7 +53,7 @@ export default {
     return new Promise((resolve, reject) => {
       pool
         .query(queries)
-        .then(res => resolve(res))
+        .then(result => resolve(result))
         .catch(err => reject(err));
     });
   },
