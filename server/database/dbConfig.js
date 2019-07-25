f434e7c8bc9ea30bb3d11664fce48054245b9d7c
vectormike40 <victorjonah199@gmail.com>
2019-07-25 15:22:13 +0100

    New db config

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -1,61 +1,13 @@
-// const Sequelize = require('sequelize');
+import knex from 'knex';
 
-// const config = new Sequelize(
-//   'eatom',
-//   'vectormike',
-//   '1234',
-
-//   {
-//     host: 'localhost',
-//     dialect: 'postgres',
-//     operatorsAliases: false,
-
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   },
-// );
-
-// export default config;
-
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const res = dotenv.config();
-
-if (res.error) {
-  throw res.error;
-  console.log('Error is ')
-}
-
-
-let connectionString;
-if (process.env.NODE_ENV === 'test') {
-  connectionString = process.env.DB_URL;
-} else if (process.env.NODE_ENV === 'development') {
-  connectionString = process.env.DB_URL;
-} else {
-  connectionString = process.env.DB_URL;
-}
-
-// Create new instance
-const pool = new Pool({ connectionString });
-// console.log(pool)
-
-
-// Query function
-export default {
-  query(queries) {
-    return new Promise((resolve, reject) => {
-      pool
-        .query(queries)
-        .then(result => resolve(result))
-        .catch(err => reject(err));
-    });
+const database = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'vectormike',
+    password: '1234',
+    database: 'eatom',
   },
-};
+});
+
+export { database };
