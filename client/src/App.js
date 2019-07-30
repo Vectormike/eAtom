d81b2689191d85513eca1f77e2e43a6cd3c961dc
vectormike40 <victorjonah199@gmail.com>
2019-07-30 15:33:16 +0100

    Imporrted show component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar/navbar';
+import Shows from './component/Shows/shows';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Shows />
     </div>
   );
 }
