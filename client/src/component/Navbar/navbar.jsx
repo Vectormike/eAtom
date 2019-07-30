8e91b23c31a72073dd9eaf002620ae44063745a3
vectormike40 <victorjonah199@gmail.com>
2019-07-30 12:27:41 +0100

    ...

--- client/src/component/Navbar/navbar.jsx
+++ client/src/component/Navbar/navbar.jsx
@@ -1,7 +1,92 @@
 import React from 'react';
 
 export default function Navbar() {
-    return (
-        <div>Hi</div>
-    )
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="to">
+        Navbar
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="tonavbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+
+      <div
+        class="collapse navbar-collapse"
+        id="navbarSupportedContent"
+      >
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="to">
+              Home <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="to">
+              Link
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="to"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <div
+              class="dropdown-menu"
+              aria-labelledby="navbarDropdown"
+            >
+              <a class="dropdown-item" href="to">
+                Action
+              </a>
+              <a class="dropdown-item" href="to">
+                Another action
+              </a>
+              <div class="dropdown-divider" />
+              <a class="dropdown-item" href="to">
+                Something else here
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a
+              class="nav-link disabled"
+              href="to"
+              tabindex="-1"
+              aria-disabled="true"
+            >
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            class="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
 }
