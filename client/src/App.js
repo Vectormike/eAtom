1fbead148e2c8fe1131aab0281c1fc855fd91396
vectormike40 <victorjonah199@gmail.com>
2019-07-31 11:21:13 +0100

    Imported footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './component/Navbar/navbar';
 import Shows from './component/Shows/shows';
+import Footer from './component/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Shows />
+      <Footer />
     </div>
   );
 }
