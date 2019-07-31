da6d081714a989937c51f7833dea94e6666a4391
vectormike40 <victorjonah199@gmail.com>
2019-07-31 11:00:34 +0100

    Finished shows.. updates left

--- client/src/component/Shows/shows.jsx
+++ client/src/component/Shows/shows.jsx
@@ -2,28 +2,48 @@ import React from 'react';
 
 export default function Show() {
   return (
-    <div className="">
-      <div className="row">
-        <div class="col-sm-3" style={{ }}>
-          <img
-            src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
-            class=""
-            alt="New Arrivals"
-          />
+    <div>
+      <div className="top container-fluid">
+        <div className="row">
+          <div className="col-md-6" style={{}}>
+            <img
+              src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+              className=""
+              alt="..."
+            />
+          </div>
+          <div className="col-md-3" style={{}}>
+            <img
+              src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+              className=""
+              alt="..."
+            />
+          </div>
         </div>
-        <div class="col-sm-4" style={{ }}>
-          <img
-            src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
-            class=""
-            alt="..."
-          />
-        </div>
-        <div class="col-sm-3" style={{ }}>
-          <img
-            src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
-            class=""
-            alt="..."
-          />
+      </div>
+      <div className="bottom container-fluid">
+        <div className="row">
+          <div class="col-md-4" style={{}}>
+            <img
+              src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+              className=""
+              alt="New Arrivals"
+            />
+          </div>
+          <div className="col-md-4" style={{}}>
+            <img
+              src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+              className=""
+              alt="..."
+            />
+          </div>
+          <div className="col-md-4" style={{}}>
+            <img
+              src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+              className=""
+              alt="..."
+            />
+          </div>
         </div>
       </div>
     </div>
