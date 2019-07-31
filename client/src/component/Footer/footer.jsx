01d35f9f7d18953bb243fae3373248483bb8e35c
vectormike40 <victorjonah199@gmail.com>
2019-07-31 15:57:03 +0100

    More div

--- client/src/component/Footer/footer.jsx
+++ client/src/component/Footer/footer.jsx
@@ -3,10 +3,60 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="container-fluid">
+      <form>
+        <div class="form-group row">
+          <div className=" col-md-4">
+            <h5>Connect</h5>
+            <ul>
+              <li>
+                <i
+                  class="fab fa-facebook"
+                  aria-hidden="true"
+                />
+              </li>
+              <li>
+                <i class="fab fa-instagram" aria-hidden="true" />
+              </li>
+              <li>
+                <i class="fab fa-pinterest" aria-hidden="true" />
+              </li>
+            </ul>
+          </div>
+
+          <div className=" col-md-4">
+            <h5>Shop</h5>
+            <ul>
+              <li>For Women</li>
+              <li>For Men</li>
+              <li>For Children</li>
+            </ul>
+          </div>
+
+          <div className=" col-md-4">
+            <label
+              for="staticEmail"
+              class="col-sm-12 col-md-4 col-form-label"
+            >
+              Email
+            </label>
+            <div class="col-sm-12 col-md-4">
+              <input
+                type="text"
+                class="form-control-plaintext"
+                id="staticEmail"
+                placeholder="Your Email Address"
+              />
+            </div>
+          </div>
+        </div>
+      </form>
       <p className="float-left">2019. All rights reserved.</p>
       <p className="float-right">Support me</p>
       <div className="fixed-bottom">
-        <i className="float-right fa fa-arrow-circle-up" aria-hidden="true" />
+        <i
+          className="float-right fa fa-arrow-circle-up"
+          aria-hidden="true"
+        />
       </div>
     </div>
   );
