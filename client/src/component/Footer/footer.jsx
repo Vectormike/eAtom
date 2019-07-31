892a7c399966dbab6565040047d12a1be95613ad
vectormike40 <victorjonah199@gmail.com>
2019-07-31 11:21:06 +0100

    Created footer

---
+++ client/src/component/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="container-fluid">
+      <p className="float-left">2019. All rights reserved.</p>
+      <p className="float-right">Support me</p>
+      <div className="fixed-bottom">
+        <i className="float-right fa fa-arrow-circle-up" aria-hidden="true" />
+      </div>
+    </div>
+  );
+}
