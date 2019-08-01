4882ba47eef82f185743c2468495f93e80be657d
vectormike40 <victorjonah199@gmail.com>
2019-08-01 13:03:21 +0100

    Route for dashboard done

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './component/Home/home';
 import Login from './component/Login/login';
+import Dashboard from './component/Dashboard/dashboard';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 }
