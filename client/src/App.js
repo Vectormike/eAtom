58409f3a1e3827c898f184cc4643f9aa9a9feaf4
vectormike40 <victorjonah199@gmail.com>
2019-08-02 10:59:33 +0100

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './component/Home/home';
 import Login from './component/Login/login';
 import Dashboard from './component/Dashboard/dashboard';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-    </Switch>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
