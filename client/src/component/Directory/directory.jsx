265250e6ca26b1658f96f4007b6d20146c9ab09d
Vectormike <victorjonah199@gmail.com>
2019-08-19 13:47:09 +0100

    Created two new Directories for the homepage

---
+++ client/src/component/Directory/directory.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import MenuItem1 from '../menuItem/menuItem1';
+import MenuItem2 from '../menuItem/menuItem2';
+
+
+export class Directory1 extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.sections.map(({ title, imageUrl, id }) => (
+          <MenuItem1 key={id} title={title} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export class Directory2 extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.sections.map(({ title, imageUrl, id }) => (
+          <MenuItem2 key={id} title={title} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+
+
