34918f54d17211e19309d3203d4654d048c6ad36
Vectormike <victorjonah199@gmail.com>
2019-08-19 16:09:03 +0100

    Fixed bug

--- client/src/component/menuItem/menuItem.jsx
+++ client/src/component/menuItem/menuItem.jsx
@@ -1,34 +1,23 @@
 import React from 'react';
+import './menuItem.scss';
 
-function MenuItem({}) {
+function MenuItem1({ title, imageUrl }) {
   return (
-    <div className="top container-fluid">
-      <div className="row">
-        <div className="col-md-6" style={{}}>
-          <img
-            src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
-            className=""
-            alt="..."
-          />
+    <div className="menu-item">        
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+            
+          }}
+        >
           <div className="content">
-            <h1>{}</h1>
-            <span>BUY</span>
-          </div>
-        </div>
-        <div className="col-md-6" style={{}}>
-          <img
-            src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
-            className=""
-            alt="..."
-          />
-          <div className="content">
-            <h1>{}</h1>
-            <span>BUY</span>
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">BUY</span>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
-export default MenuItem;
+export default MenuItem1;
