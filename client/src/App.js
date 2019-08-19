7c451d836d944b11f72378afbb1fb81fb236e0d4
Vectormike <victorjonah199@gmail.com>
2019-08-19 11:19:27 +0100

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './component/Home/home';
+
+import HomePage from './component/pages/HomePage/homepage';
 import Login from './component/Login/login';
 import Dashboard from './component/Dashboard/dashboard';
 
 class App extends Component {
-
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
     );
   }
 }
