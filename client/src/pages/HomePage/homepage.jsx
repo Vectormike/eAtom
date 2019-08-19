2e7c7cbdbb11265a44ad9fbc4dbf5281b6b6cc5a
Vectormike <victorjonah199@gmail.com>
2019-08-19 16:09:47 +0100

    Homepage now

---
+++ client/src/pages/HomePage/homepage.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Navbar from '../../component/Navbar/navbar';
+import { Directory } from '../../component/Directory/directory';
+import Footer from '../../component/Footer/footer';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <Directory />
+      <Footer />
+    </div>
+  );
+}
+
+// <div className="top container-fluid">
+// <div className="row">
+//   <div className="col-md-6" style={{}}>
+//     <img
+//       src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+//       className=""
+//       alt="..."
+//     />
+//   </div>
+//   <div className="col-md-6" style={{}}>
+//     <img
+//       src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+//       className=""
+//       alt="..."
+//     />
+//   </div>
+// </div>
+// </div>
+// <div className="bottom container-fluid">
+// <div className="row">
+//   <div class="col-md-4" style={{}}>
+//     <img
+//       src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+//       className=""
+//       alt="New Arrivals"
+//     />
+//   </div>
+//   <div className="col-md-4" style={{}}>
+//     <img
+//       src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+//       className=""
+//       alt="..."
+//     />
+//   </div>
+//   <div className="col-md-4" style={{}}>
+//     <img
+//       src="https://res.cloudinary.com/vectormike/image/upload/v1564498173/new-arrivals.jpg"
+//       className=""
+//       alt="..."
+//     />
+//   </div>
+// </div>
+// </div>
