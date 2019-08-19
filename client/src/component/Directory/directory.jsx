e856a9715e5d71b5290a1c8a4f4535e72f0ae51c
Vectormike <victorjonah199@gmail.com>
2019-08-19 16:08:33 +0100

    ...

--- client/src/component/Directory/directory.jsx
+++ client/src/component/Directory/directory.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import MenuItem1 from '../menuItem/menuItem1';
-import MenuItem2 from '../menuItem/menuItem2';
+import MenuItem from '../../component/menuItem/menuItem';
 
+import './directory.scss';
 
-export class Directory1 extends Component {
+export class Directory extends Component {
   constructor() {
     super();
 
@@ -41,60 +41,11 @@ export class Directory1 extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="directory-menu">
         {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem1 key={id} title={title} imageUrl={imageUrl} />
+          <MenuItem key={id} title={title} imageUrl={imageUrl} />
         ))}
       </div>
     );
   }
 }
-
-export class Directory2 extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-        },
-      ],
-    };
-  }
-  render() {
-    return (
-      <div>
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem2 key={id} title={title} imageUrl={imageUrl} />
-        ))}
-      </div>
-    );
-  }
-}
-
-
