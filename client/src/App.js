46b472eef67cbd23a06b906a894b9c94cd9f1e2d
Vectormike <victorjonah199@gmail.com>
2019-08-26 14:47:45 +0100

    state for user, passed currentuser props to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import Navbar from './component/Navbar/navbar';
 import HomePage from './pages/HomePage/homepage';
 import Shop from './pages/Shop/shop';
 import Login from './pages/LoginRegister/loginRegister';
 import Dashboard from './pages/Dashboard/dashboard';
+import { auth } from './services/firebase/firebase.utils';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      presentUser: null,
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ presentUser: user });
+
+      console.log(user);
+    });
+  }
+
   render() {
     return (
       <div>
+        <Navbar presentUser={this.state.presentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={Shop} />
