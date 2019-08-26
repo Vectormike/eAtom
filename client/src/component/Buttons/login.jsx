5b305f330004f0110f50083ce092018dc8da2e23
Vectormike <victorjonah199@gmail.com>
2019-08-26 10:01:02 +0100

    ...

--- client/src/component/Buttons/login.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LoginButton({ children, ...otherProps }) {
-  return (
-    <button className="custom-button" otherProps>
-      {children}
-    </button>
-  );
-}
