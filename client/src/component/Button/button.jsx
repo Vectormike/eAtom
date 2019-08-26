a178c1b0c59881a2a25fce099b8749f6df60664f
Vectormike <victorjonah199@gmail.com>
2019-08-26 16:24:29 +0100

    Google sign in button with Google

--- client/src/component/Button/button.jsx
+++ client/src/component/Button/button.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './button.scss'
+import './button.scss';
 
-
-export default function LoginButton({ children, ...otherProps }) {
+export default function LoginButton({
+  children,
+  isGoogleSignIn,
+  ...otherProps
+}) {
   return (
-    <button className="custom-button" {...otherProps}>
+    <button
+      className={`${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      } custom-button`}
+      {...otherProps}
+    >
       {children}
     </button>
   );
