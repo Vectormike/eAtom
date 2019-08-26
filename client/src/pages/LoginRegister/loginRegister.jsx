449d754a45ac34fc46709b8fdd676ba279bd1d24
Vectormike <victorjonah199@gmail.com>
2019-08-26 11:35:08 +0100

    ...

--- client/src/pages/LoginRegister/loginRegister.jsx
+++ client/src/pages/LoginRegister/loginRegister.jsx
@@ -1,66 +1,34 @@
-import React, { Component } from 'react';
+import React   from 'react';
 
 import Login from '../../component/Login/login';
 // import Footer from '../../component/Footer/footer';
 // import Navbar from '../../component/Navbar/navbar';
 
-class LoginRegister extends Component {
-  constructor() {
-    super();
+const LoginRegister = () => {
+  // onRegisterSubmit = e => {
+  //   e.preventDefault();
+  //   fetch('http://localhost:5000/register', {
+  //     method: 'post',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({
+  //       lastname: this.state.lastname,
+  //       firstname: this.state.firstname,
+  //       email: this.state.email,
+  //       password: this.state.password,
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data === 'User Created') {
+  //       }
+  //     });
+  // };
 
-    this.state = {
-      lastname: '',
-      firstname: '',
-      email: '',
-      password: '',
-      user: false,
-    };
-  }
-
-  onRegisterSubmit = e => {
-    e.preventDefault();
-    fetch('http://localhost:5000/register', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        lastname: this.state.lastname,
-        firstname: this.state.firstname,
-        email: this.state.email,
-        password: this.state.password,
-      }),
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data === 'User Created') {
-        }
-      });
-  };
-
-  onLoginSubmit = e => {
-    e.preventDefault();
-    fetch('http://localhost:5000/login', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        email: this.state.signInEmail,
-        password: this.state.signInPassword,
-      }),
-    })
-      .then(response => response.json())
-      .then(console.log);
-  };
- 
-  onRoutChange = () => {
-    this.props.history.push('/dashboard');
-  };
-
-  render() {
-    return (
-      <div className="loginRegister">
-        <Login />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="loginRegister">
+      <Login />
+    </div>
+  );
+};
 
 export default LoginRegister;
