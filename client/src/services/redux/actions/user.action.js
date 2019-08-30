35fdaee11e01de227954b7ae0ec691470cbec2b6
Vectormike <victorjonah199@gmail.com>
2019-08-30 15:54:37 +0100

    Action for the user

--- client/src/services/redux/actions/user.action.js
+++ client/src/services/redux/actions/user.action.js
@@ -1,8 +1,8 @@
-import {SET_CURRENT_USER} from '../constant'
+import { SET_CURRENT_USER } from '../constant';
 
-export const setCurrentUser = (user) => async dispatch => {
-    dispatch({
-        type: SET_CURRENT_USER,
-        payload: user
-    })
-}
+export const setCurrentUser = user => async dispatch => {
+  dispatch({
+    type: SET_CURRENT_USER,
+    payload: user,
+  });
+};
