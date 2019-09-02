d14fba15874be86a61053cb2ba8206fe096d8cc4
Vectormike <victorjonah199@gmail.com>
2019-09-02 15:20:01 +0100

    Combined cart reducer

--- client/src/services/redux/reducer/root.reducer.js
+++ client/src/services/redux/reducer/root.reducer.js
@@ -1,4 +1,8 @@
 import { combineReducers } from 'redux';
-import userReducer from '../reducer/user.reducer';
+import { userReducer } from '../reducer/user.reducer';
+import { cartReducer } from '../reducer/cart.reducer';
 
-export default combineReducers({ user: userReducer });
+export default combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
