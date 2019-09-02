0030a327ec214d8d08a73e79063b2303283be962
Vectormike <victorjonah199@gmail.com>
2019-09-02 15:16:29 +0100

    Connected required action

--- client/src/component/CartIcon/cart-icon.jsx
+++ client/src/component/CartIcon/cart-icon.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import ShoppingIcon from '../resources/market.svg';
+import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 
-const CartIcon = () => (
-  <div className="">
-    <ShoppingIcon />
-  </div>
-);
+import { cartAction } from '../../services/redux/actions/cart.action.js';
+import { connect } from 'react-redux';
+import './cart-icon.scss';
+
+function CartIcon({ toggleModal }) {
+  return (
+    <div className="cart-icon" onClick={toggleModal}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">0</span>
+    </div>
+  );
+}
+
+const mapDispatchToProps = dispatch => ({
+  toggleModal: () => dispatch(cartAction()),
+});
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(CartIcon);
