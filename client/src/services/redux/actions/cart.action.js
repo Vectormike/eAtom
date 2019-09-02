a33c931d98131b3c612e95fd0b37f2c2cab8d6f6
Vectormike <victorjonah199@gmail.com>
2019-09-02 19:19:14 +0100

    More action for cart

--- client/src/services/redux/actions/cart.action.js
+++ client/src/services/redux/actions/cart.action.js
@@ -1,5 +1,10 @@
-import { TOGGLE_CART } from '../constant';
+import { TOGGLE_CART, ADD_ITEM } from '../constant';
 
 export const cartAction = () => ({
-    type: TOGGLE_CART
+  type: TOGGLE_CART,
+});
+
+export const addItem = (item) => ({
+    type: ADD_ITEM,
+    payload: item
 })
