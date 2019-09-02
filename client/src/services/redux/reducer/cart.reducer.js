8f08624c5c8c14debef36562cbfdf3fc852b7df3
Vectormike <victorjonah199@gmail.com>
2019-09-02 19:19:31 +0100

    Reducer for addItem

--- client/src/services/redux/reducer/cart.reducer.js
+++ client/src/services/redux/reducer/cart.reducer.js
@@ -1,15 +1,21 @@
-import { TOGGLE_CART } from '../constant';
+import { TOGGLE_CART, ADD_ITEM } from '../constant';
 
 const initialState = {
   hidden: true,
+  cartItems: [],
 };
 
-export const cartReducer = (state = initialState, action) => {
+export const cartReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case TOGGLE_CART:
       return {
         state,
         hidden: !state.hidden,
+      };
+    case ADD_ITEM:
+      return {
+        state,
+        cartItems: [...state.cartItems, action.payload],
       };
     default:
       return state;
