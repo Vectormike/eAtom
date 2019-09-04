f95a413bdbdcae013cfe839804a5079ac9cffd6e
Vectormike <victorjonah199@gmail.com>
2019-09-04 16:02:11 +0100

    Brought in state

--- client/src/component/CartModal/cart-modal.jsx
+++ client/src/component/CartModal/cart-modal.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-
 import Button from '../Button/button';
+import CartItems from '../CartItems/cart-items';
+import { connect } from 'react-redux';
 
-import './cart-modal.scss'
+import './cart-modal.scss';
 
-export default function CartModal() {
+function CartModal({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItems key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <Button>PAY</Button>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  cartItems: state.cartItems,
+});
+
+export default connect(mapStateToProps)(CartModal);
