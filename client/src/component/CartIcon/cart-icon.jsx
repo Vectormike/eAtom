28f24297c49fe6bdf9bbad171cc13daf947c74fb
Vectormike <victorjonah199@gmail.com>
2019-09-05 13:32:41 +0100

    Added quantity of items to be displayed

--- client/src/component/CartIcon/cart-icon.jsx
+++ client/src/component/CartIcon/cart-icon.jsx
@@ -6,20 +6,27 @@ import { cartAction } from '../../services/redux/actions/cart.action.js';
 import { connect } from 'react-redux';
 import './cart-icon.scss';
 
-function CartIcon({ toggleModal }) {
+function CartIcon({ toggleModal, count }) {
   return (
     <div className="cart-icon" onClick={toggleModal}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{count}</span>
     </div>
   );
 }
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  count: cartItems.reduce((acc, items) => (
+    acc + items.quantity
+  ), 0)
+})
+
 
 const mapDispatchToProps = dispatch => ({
   toggleModal: () => dispatch(cartAction()),
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(CartIcon);
