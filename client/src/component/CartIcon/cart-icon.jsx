e3e316f48824ef8244b413c7c0b052c9ac15d2a0
Vectormike <victorjonah199@gmail.com>
2019-09-06 15:07:01 +0100

    Added count func

--- client/src/component/CartIcon/cart-icon.jsx
+++ client/src/component/CartIcon/cart-icon.jsx
@@ -4,23 +4,22 @@ import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 
 import { cartAction } from '../../services/redux/actions/cart.action.js';
 import { connect } from 'react-redux';
+import { selectCartItemsCount } from '../../services/redux/cart.selectors';
+
 import './cart-icon.scss';
 
 function CartIcon({ toggleModal, count }) {
   return (
-    <div className="cart-icon" onClick={toggleModal}>
+    <div className="cart-icon" onMouseOver={toggleModal} >
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">{count}</span>
     </div>
   );
 }
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  count: cartItems.reduce((acc, items) => (
-    acc + items.quantity
-  ), 0)
-})
-
+const mapStateToProps = state => ({
+  count: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleModal: () => dispatch(cartAction()),
