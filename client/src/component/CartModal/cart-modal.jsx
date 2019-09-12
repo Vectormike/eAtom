e3d4d48b06f3eb969932423217274eae879a4eda
Vectormike <victorjonah199@gmail.com>
2019-09-12 13:05:19 +0100

    Added event click to checkout page.

--- client/src/component/CartModal/cart-modal.jsx
+++ client/src/component/CartModal/cart-modal.jsx
@@ -3,18 +3,23 @@ import Button from '../Button/button';
 import CartItems from '../CartItems/cart-items';
 import { selectCartItems } from '../../services/redux/cart.selectors';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import './cart-modal.scss';
 
-function CartModal({ cartItems }) {
+function CartModal({ cartItems, history }) {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map(items => (
-          <CartItems key={items.id} item={items} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map(items => (
+            <CartItems key={items.id} item={items} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
-      <Button>PAY</Button>
+      <Button onClick={() => history.push('/checkout')}>PAY</Button>
     </div>
   );
 }
@@ -23,4 +28,4 @@ const mapStateToProps = state => ({
   cartItems: selectCartItems(state),
 });
 
-export default connect(mapStateToProps)(CartModal);
+export default withRouter(connect(mapStateToProps)(CartModal));
