e32d50b0806bbc0a42081df457d00ef0aa5e1883
Vectormike <victorjonah199@gmail.com>
2019-09-12 17:27:16 +0100

    Check out items component

---
+++ client/src/component/CheckoutItem/checkout-item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './checkout-item.scss';
+
+export default function CheckoutItem({items: {name, quantity, price, imageUrl}}) {
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <span className="remove-button">&#10005;</span>
+    </div>
+  );
+}
