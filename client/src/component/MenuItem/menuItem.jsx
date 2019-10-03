18fa67a66ad30f9c48f0ce74b83b684680fcf496
Vectormike <victorjonah199@gmail.com>
2019-10-03 16:52:57 +0100

    HOC

--- client/src/component/MenuItem/menuItem.jsx
+++ client/src/component/MenuItem/menuItem.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menuItem.scss';
 
-function MenuItem1({ title, imageUrl }) {
+function MenuItem1({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match,
+}) {
   return (
-    <div className="menu-item">        
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-            
-          }}
-        >
-          <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">BUY</span>
-          </div>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      >
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">BUY</span>
         </div>
       </div>
+    </div>
   );
 }
 
-export default MenuItem1;
+export default withRouter(MenuItem1);
