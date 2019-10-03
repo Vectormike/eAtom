5c1915adcd3069cff4722296c9b2cc1c89a001d6
Vectormike <victorjonah199@gmail.com>
2019-10-03 16:50:14 +0100

    Updated

--- client/src/services/redux/reducer/directory.reducer.js
+++ client/src/services/redux/reducer/directory.reducer.js
@@ -4,28 +4,33 @@ const INITIAL_STATE = {
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       id: 1,
+      linkUrl: 'hats',
     },
     {
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       id: 2,
+      linkUrl: 'jackets',
     },
     {
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       id: 3,
+      linkUrl: 'sneakers',
     },
     {
       title: 'womens',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
+      linkUrl: 'womens',
     },
     {
       title: 'mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
+      linkUrl: 'mens',
     },
   ],
 };
